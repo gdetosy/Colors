966e1729701beecb4fa06d1262515732a347ecbd
gdetosy <tosyjdm@gmail.com>
2022-11-30 19:24:44 +0300

    start

--- Color/Color/ViewController.swift
+++ Color/Color/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var changeBGColorButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func changeBgColorButton(_ sender: Any) {
+        
+     performSegue(withIdentifier: "colorsViewController", sender: nil)
+        
+        
+    }
+    
 
 }
 
